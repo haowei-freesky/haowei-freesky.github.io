a898b808f3086eaa630264350ee493cf01f1035e
haowei-freesky <110572501+haowei-freesky@users.noreply.github.com>
2025-01-11 15:04:21 +0000

    Deploying to gh-pages from @ haowei-freesky/haowei-freesky.github.io@f0e28f94df4a9f01a8c2c233953295edc2814f6d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-service",
+          title: "Service",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/service/";
+          },
         },{id: "nav-repositories",
           title: "Repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
